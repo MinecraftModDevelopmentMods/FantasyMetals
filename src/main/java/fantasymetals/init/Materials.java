878ab8db93b8629933d6b1dbf7c1468ea0742788
Jasmine Iwanek <jasmine@iwanek.co.uk>
2016-07-10 14:20:47 -0700

    Add initial content hints

--- src/main/java/fantasymetals/init/Materials.java
+++ src/main/java/fantasymetals/init/Materials.java
@@ -12,6 +12,10 @@ import cyano.basemetals.material.MetalMaterial;
 public abstract class Materials extends cyano.basemetals.init.Materials {
 
 	public static MetalMaterial template;
+	public static MetalMaterial enderium;
+	public static MetalMaterial lumium;
+	public static MetalMaterial prismarinium;
+	public static MetalMaterial signalum;
 
 	private static boolean initDone = false;
 
@@ -23,6 +27,10 @@ public abstract class Materials extends cyano.basemetals.init.Materials {
 
 		// Mod Metals
 		template = addMaterial("template", 8, 8, 4.5);
+		enderium = addMaterial("enderium", 8, 8, 4.5);
+		lumium = addMaterial("lumium", 8, 8, 4.5);
+		prismarinium = addMaterial("prismarinium", 8, 8, 4.5);
+		signalum = addMaterial("signalum", 8, 8, 4.5);
 
 		initDone = true;
 	}
