adacf8344d48f5b618b151528beca1dfdf4d378c
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-02-09 19:44:08 -0800

    Cleaning

--- src/main/java/com/mcmoddev/fantasymetals/init/Recipes.java
+++ src/main/java/com/mcmoddev/fantasymetals/init/Recipes.java
@@ -1,6 +1,7 @@
 package com.mcmoddev.fantasymetals.init;
 
 import com.mcmoddev.fantasymetals.util.Config.Options;
+import com.mcmoddev.lib.util.Oredicts;
 
 import net.minecraft.item.*;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -38,20 +39,20 @@ public class Recipes extends com.mcmoddev.lib.init.Recipes {
 
 	private static void initModSpecificRecipes() {
 		if (Options.enableEnderium) {
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.enderium.blend, 2), "dustEnderium", "dustEnderium"));
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.enderium.smallblend, 2), "smalldustEnderium", "smalldustEnderium"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.enderium.blend, 2), Oredicts.DUST + "Enderium", Oredicts.DUST + "Enderium"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.enderium.smallblend, 2), Oredicts.DUSTTINY + "Enderium", Oredicts.DUSTTINY + "Enderium"));
 		}
 		if (Options.enableLumium) {
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.lumium.blend, 2), "dustLumium", "dustLumium"));
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.lumium.smallblend, 2), "smalldustLumium", "smalldustLumium"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.lumium.blend, 2), Oredicts.DUST + "Lumium", Oredicts.DUST + "Lumium"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.lumium.smallblend, 2), Oredicts.DUSTTINY + "Lumium", Oredicts.DUSTTINY + "Lumium"));
 		}
 		if (Options.enablePrismarinium) {
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.prismarinium.blend, 2), "dustEnderium", "dustSignalum"));
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.prismarinium.smallblend, 2), "smalldustPrismarinium", "smalldustPrismarinium"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.prismarinium.blend, 2), Oredicts.DUST + "Enderium", Oredicts.DUST + "Signalum"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.prismarinium.smallblend, 2), Oredicts.DUSTTINY + "Prismarinium", Oredicts.DUSTTINY + "Prismarinium"));
 		}
 		if (Options.enableSignalum) {
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.signalum.blend, 2), "dustSignalum", "dustSignalum"));
-			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.signalum.smallblend, 2), "smalldustSignalum", "smalldustSignalum"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.signalum.blend, 2), Oredicts.DUST + "Signalum", Oredicts.DUST + "Signalum"));
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Materials.signalum.smallblend, 2), Oredicts.DUSTTINY + "Signalum", Oredicts.DUSTTINY + "Signalum"));
 		}
 	}
 }
